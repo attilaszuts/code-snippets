c6dc4fe8fae0c01c76505eb88c5a2e7df10d4f86
szutsattila <szuts.attila97@gmail.com>
2021-02-12 11:04:48 +0100

    add trycatch

--- discord-webhook/discord-webhook.R
+++ discord-webhook/discord-webhook.R
@@ -1,21 +1,29 @@
 # define webhook function
-send_message <- function(webhookurl, my_text) {
+send_message <- function(webhookurl = url, my_text = text) {
   library(jsonlite)
   library(rvest)
   
   headers = c(
     `Content-type` = 'application/json'
   )
-  data= toJSON(list("content"= my_text), auto_unbox = T)
-  res <- httr::POST(url = webhookurl, httr::add_headers(.headers=headers), body = data)
+  tryCatch({
+    data= toJSON(list("content"= my_text), auto_unbox = T)
+    res <- httr::POST(url = webhookurl, httr::add_headers(.headers=headers), body = data)
+  },
+  warning = function(w) {
+    print('warning!')
+  }, error = function(e) {
+    print('error!')
+  }, finally = {
+  }
+  )
 }
+
 
 # read in credentials
 creds <- read.delim('/mnt/DATA/Projects/BA-20-21/danalysis-3/data/creds.txt', sep = ',') # change the path to your creds.txt file
-webhookurl <- as.character(creds[creds$key == 'webhookurl', 2])
+url <- as.character(creds[creds$key == 'webhookurl', 2])
 user <- as.character(creds[creds$key == 'user', 2])
 
-my_text <- paste0('hello ', user, ' your model has finished training')
-
-# send_message(webhookurl, my_text)
+text <- paste0('hello ', user, ' your model has finished training!')
 
