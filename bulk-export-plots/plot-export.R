81ab0354e5a3d18ac993b479dd3a15c732c8d437
szutsattila <szuts.attila97@gmail.com>
2021-03-01 20:34:07 +0100

    update plot export to wrok in different environments

--- bulk-export-plots/plot-export.R
+++ bulk-export-plots/plot-export.R
@@ -1,13 +1,23 @@
-# export plots
-plot_ind <- grep("^pl_", ls())
-export_obj <- ls()[plot_ind]
-
-for (plot in export_obj) {
-  tryCatch({ggsave(paste0("out/", plot, ".png"), eval(parse(text = plot)), )},
-           error = function(e) {print(paste0('error with plot: ', plot))},
-           finally = {})
+# export ggplots
+plot_export <- function() {
+  wd <- getwd()
+  plot_ind <- grep("^pl_", ls(pos = 1))
+  export_obj <- ls(pos = 1)[plot_ind]
+  
+  print(export_obj)
+  
+  # print("good1")
+  # print(parent.env(globalenv()))
+  # print("good2")
+  
+  for (plot in export_obj) {
+    tryCatch({ggsave(paste0("out/", plot, ".png"), eval(parse(text = plot)), )},
+             error = function(e) {print(paste0('error with plot: ', plot))},
+             finally = {})
+  }
 }
 
+# export not ggplots
 plot_out <- function(plot_obj) {
   name <- deparse(substitute(plot_obj))
   png(file=paste0("out/", name, ".png"),
